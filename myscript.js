9fc71ab78d4c3ec1cb9d0c137ca66d19f75ebf98
mahedi5061 <mahedi5061@gmail.com>
2021-01-27 20:36:36 +0600

    update

---
+++ myscript.js
@@ -0,0 +1,5 @@
+var loginBtn=document.getElementById('enter-login');
+loginBtn.addEventListener('click',function(){
+     const loginHide=document.getElementById('login-area');
+     loginHide.style.display="none";
+})
